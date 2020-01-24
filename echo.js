b02fd9e4ed75ce41ad336dc307ed09d804fc90e6
Jon <22411356+jjhna@users.noreply.github.com>
2020-01-23 17:26:16 -1000

    added more files

---
+++ echo.js
@@ -0,0 +1,12 @@
+var websocket = require("./websocket-example");
+websocket.listen(9999, "localhost", function(conn) {   
+    console.log("connection opened");
+    conn.on("data", function(opcode, data) {      
+        console.log("message: ", data);      
+        conn.send(data);   
+    });   
+    
+    conn.on("close", function(code, reason) {      
+        console.log("connection closed: ", code, reason);   
+    });
+});
